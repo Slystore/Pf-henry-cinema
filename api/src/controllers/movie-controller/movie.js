f6450c14e6d41642836f2044cbd4c45bf8165b10
ThestalkerN-byte <82970148+ThestalkerN-byte@users.noreply.github.com>
2021-09-29 12:11:11 -0300

    movies crud semi-complete
    falta corregir el post con respecto a los generos

--- api/src/controllers/movie-controller/movie.js
+++ api/src/controllers/movie-controller/movie.js
@@ -1,4 +1,3 @@
-
 const { movies, genres } = require("../../db");
 
 const moviePost = async (req, res) => {
@@ -38,28 +37,39 @@ const moviePost = async (req, res) => {
   }
 };
 
-const movieDelete = async (req,res) =>{
-  try{
-    const {id} = req.params
+const movieDelete = async (req, res) => {
+  try {
+    const { id } = req.params;
     const movieId = await movies.findByPk(id);
-    console.log('esta es la id',movieId)
-    if(movieId){
+    console.log("esta es la id", movieId);
+    if (movieId) {
       movieId.destroy();
-      res.json("Movie delete succesfully")
-    }else res.status(404).json("This movie doesn't exist")
-  }catch(err){
-    console.log(err)
+      res.json("Movie delete succesfully");
+    } else res.status(404).json("This movie doesn't exist");
+  } catch (err) {
+    console.log(err);
   }
-}
-const moviePut = async(req,res) =>{
-  try{
-    
-  }catch(err){
-    console.log(err)
+};
+const moviePut = async (req, res) => {
+  const infoMovie = req.body;
+
+  try {
+    const { id } = req.params;
+
+    const updateMovie = await movies.update(infoMovie, {
+      where: {
+        id:id,
+      },
+    });
+    if(updateMovie.length !== 0) return res.status(200).send(updateMovie)
+    else res.status(404).json('Movie Not found')
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
 module.exports = {
   moviePost,
-  movieDelete
+  movieDelete,
+  moviePut,
 };
--- api/src/routes/movies/movies.js
+++ api/src/routes/movies/movies.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {moviePost, movieDelete} = require('../../controllers/movie-controller/movie')
+const {moviePost, movieDelete, moviePut} = require('../../controllers/movie-controller/movie')
 const router = express.Router()
 
 router.post('/createMovie',moviePost)
 router.delete('/deleteMovie/:id',movieDelete)
+router.put('/editMovie/:id', moviePut)
 
 module.exports = router
