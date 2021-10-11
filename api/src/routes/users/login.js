bd713c55bafa966e0f7025f8a801ddec2643f3e9
Slystore <91328408+Slystore@users.noreply.github.com>
2021-10-11 11:12:37 -0300

    Delete api directory

--- api/src/routes/users/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {singIn,singUp} = require('../../controllers/login-controller/login-controller')
-
-
-router.post('/singUp',singUp)
-router.post('/singIn',singIn)
-
-module.exports = router
