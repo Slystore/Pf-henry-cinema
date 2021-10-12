98e83e701ebaf6db47b5ddfeebb18668dd52e5f3
juanitu82 <76759029+juanitu82@users.noreply.github.com>
2021-10-11 23:22:34 -0300

    Bug fix in client. NavBar component

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,34 +1,45 @@
 
-import * as React from 'react';
-import {Link} from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Sidebar from '../Sidebar/Sidebar.jsx';
-import SearchBar from './SearchBar';
-import { makeStyles } from '@mui/styles';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Sidebar from "../Sidebar/Sidebar.jsx";
+import SearchBar from "./SearchBar";
+import { makeStyles } from "@mui/styles";
+import { useEffect, useState } from "react";
+import { getToken } from "../../redux/users/usersAction.js";
 
 const useStyle = makeStyles({
   botonHover: {
-    '&:hover': {      
+    "&:hover": {
       color: "#FF8E53",
-      cursor: 'pointer'
-    }
-  }
+      cursor: "pointer",
+    },
+  },
 });
 
-
 function NavBar() {
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    const x = getToken();
+    if (x.msg) {
+      return setUser(false);
+    } else {
+      setUser(true);
+    }
+  }, []);
 
   const classes = useStyle();
 
@@ -37,136 +48,160 @@ function NavBar() {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  
+
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  
+
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
-  
 
   const handleMenuClose = () => {
     setAnchorEl(null);
     handleMobileMenuClose();
   };
 
-  
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
-    
-  const menuId = 'primary-search-account-menu';
 
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
+  const menuId = "primary-search-account-menu";
 
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-              }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-
-      <MenuItem onClick={handleMenuClose}>Mi Perfil</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Mi Cuenta</MenuItem>
-    </Menu>
+  const renderMenu = user && (
+    <div>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleMenuClose}>Mi Perfil</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Mi Cuenta</MenuItem>
+      </Menu>
+    </div>
   );
-    
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
 
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 3 new notifications"
+          color="inherit"
+        >
+          <Badge color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="absolute"
+          style={{ background: "transparent", boxShadow: "none" }}
+        >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <Sidebar />
+            </IconButton>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" }, fontWeight: 100 }}
+            >
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Cinem
+              </Link>
+            </Typography>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                APP
+              </Link>
+            </Typography>
 
-       
-        <MenuItem>
-          <IconButton size="large" aria-label="show 3 new notifications" color="inherit" >
-            <Badge color="error">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={handleProfileMenuOpen}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-      </Menu>
-    );
-    return (
-        <div>
-            
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="absolute" style={{ background: 'transparent', boxShadow: 'none'}}>
-                    <Toolbar>
-                        <IconButton size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }} >
-                            <Sidebar />
-                        </IconButton>
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="div"
-                            sx={{ display: { xs: 'none', sm: 'block' }, fontWeight: 100}}
-                        >
-                        <Link to="/" style={{ textDecoration : 'none', color: 'white'}}>Cinem</Link>
-                        </Typography>
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="div"
-                            sx={{ display: { xs: 'none', sm: 'block' }}}
-                        >
-                        <Link to="/" style={{ textDecoration : 'none', color: 'white'}}>APP</Link>
-                        </Typography>
-                        
-                        <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ flexGrow: 1 }} />
 
-                        <Box sx={{ display: { xs: 'none', md: 'flex' }, fontWeight: 900}}>
-                        
-                          <SearchBar/>
-                          <Link to={`/shoppingCart`} >
-                          <IconButton size="large" aria-label="notifications" color="inherit"  >
-                              <Badge badgeContent={cart.length} color="error">
-                                  <ShoppingCartIcon className={ classes.botonHover } />
-                              </Badge>
-                          </IconButton>
-                          </Link>
-                            <IconButton size="large" edge="end" aria-label="account of current user" aria-controls={menuId} aria-haspopup="true" onClick={handleProfileMenuOpen} color="inherit" >
-                                <AccountCircle className={ classes.botonHover } />
-                            </IconButton>
-                        </Box>
+            <Box sx={{ display: { xs: "none", md: "flex" }, fontWeight: 900 }}>
+              <SearchBar />
+              <Link to={`/shoppingCart`}>
+                <IconButton
+                  size="large"
+                  aria-label="notifications"
+                  color="inherit"
+                >
+                  <Badge badgeContent={cart.length} color="error">
+                    <ShoppingCartIcon className={classes.botonHover} />
+                  </Badge>
+                </IconButton>
+              </Link>
+              {user && (
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  <AccountCircle className={classes.botonHover} />
+                </IconButton>
+              )}
+            </Box>
 
             <Box sx={{ display: { xs: "flex", md: "none" } }}>
               <IconButton
