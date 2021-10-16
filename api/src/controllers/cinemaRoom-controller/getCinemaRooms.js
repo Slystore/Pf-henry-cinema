8e1557802c99962a05dd922ef7d6bdd72a379578
Slystore <gabriel776977@gmail.com>
2021-10-16 16:30:30 -0300

    getCinemaRooms by Id router added, * logic in controller

--- api/src/controllers/cinemaRoom-controller/getCinemaRooms.js
+++ api/src/controllers/cinemaRoom-controller/getCinemaRooms.js
@@ -1,14 +1,23 @@
-const { cinemaRoom } = require('../../db.js');
-const { op } = require('sequelize');
+const { cinemaRoom } = require("../../db.js");
+const { op } = require("sequelize");
 
 const getCinemaRooms = async(req, res, next) => {
     try {
-        const allCinemaRooms = await cinemaRoom.findAll()
+        const { id } = req.params;
 
-        return res.status(200).json(allCinemaRooms);
+        if (id) {
+            const cinemaRoomById = await cinemaRoom.findByPk(id);
+            cinemaRoomById
+                ?
+                res.status(200).json(cinemaRoomById) :
+                res.status(404).send("No se encontro el Cinema Solicitado ");
+        } else {
+            const allCinemaRooms = await cinemaRoom.findAll();
+            return res.status(200).json(allCinemaRooms);
+        }
     } catch (err) {
-        next(err)
+        next(err);
     }
-}
+};
 
 module.exports = getCinemaRooms;
--- api/src/routes/cinemaRooms/cinemaRooms.js
+++ api/src/routes/cinemaRooms/cinemaRooms.js
@@ -4,5 +4,6 @@ const router = Router();
 const getCinemaRooms = require("../../controllers/cinemaRoom-controller/getCinemaRooms.js");
 
 router.get("/", getCinemaRooms);
+router.get("/:id", getCinemaRooms);
 
 module.exports = router;
