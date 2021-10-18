2f6ff0daeb78e3a14fcf0cd8f432d2a25f4f181a
Slystore <gabriel776977@gmail.com>
2021-10-18 12:12:36 -0300

    mocks, controllers, and purchaseMovie duplicated deleted

--- api/src/models/purchaseOrder.js
+++ api/src/models/purchaseOrder.js
@@ -2,15 +2,12 @@ const { DataTypes, UUIDV4 } = require("sequelize");
 
 module.exports = (Sequelize) => {
 
-  return Sequelize.define(
-    'purchaseOrder',{
-      fecha: {
-        type: DataTypes.DATEONLY,
-        defaultValue: DataTypes.NOW
-      },
-      // purchaseId: {
-      //   type: DataTypes.ARRAY(DataTypes.STRING)
-      // }
-    },{}
-  )
+    return Sequelize.define(
+        'purchaseOrder', {
+            fecha: {
+                type: DataTypes.DATEONLY,
+                defaultValue: DataTypes.NOW
+            },
+        }, {}
+    )
 };
